716f159b569da38f9ebc1b4a379417cd76897324
matheushenriquereiter <mathenriquereiter@gmail.com>
2024-02-12 07:54:13 -0300

    insert "(" after select function or method

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+-----------------------------------------------------------
+-- Autopairs configuration file
+----------------------------------------------------------
+
+-- Insert `(` after select function or method item
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
